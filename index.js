d5d8779bf0b9126c7aaec0231cd024184a691131
Jason Safaiyeh <jsafaiyeh@sumologic.com>
2019-12-09 20:55:52 -0800

    Added GraphQL fetch api

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+function fetchGraphQL(url, headers, query, variables) {
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers
+            },
+            body: JSON.stringify({ query, variables })
+        })
+        .then(response => resolve(response.json()))
+        .catch(error => reject(error));
+    })
 }
+
+export default fetchGraphQL;
